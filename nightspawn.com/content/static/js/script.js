107666ff17d8d9691e801f597c0819f84f3afa57
nsn <nsn@pneis.(none)>
2011-11-20 00:25:11 +0100

    nav images

--- nightspawn.com/content/static/js/script.js
+++ nightspawn.com/content/static/js/script.js
@@ -9,3 +9,25 @@ $(document).ready(function(){
 });
 
 
+$(".navlink").hover(
+  function(e) {
+    showNavImage(e.target.id);
+  }, 
+  function(e) {
+    hideNavImage(e.target.id);
+  }
+);
+
+function showNavImage(name) {
+  sel = "#" + name + "-";
+  $(sel + "gray").show();
+  $(sel + "color").show();
+  $("#nav_block-color").show();
+}
+
+function hideNavImage(name) {
+  sel = "#" + name + "-";
+  $(sel + "gray").hide();
+  $(sel + "color").hide();
+  $("#nav_block-color").hide();
+}
