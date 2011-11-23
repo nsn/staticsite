1ea7613893e6d4dfb630fc26fdf8ca1c106a31bf
nsn <nsn@pneis.(none)>
2011-11-23 16:13:20 +0100

    tag page payout

--- nightspawn.com/lib/helpers.rb
+++ nightspawn.com/lib/helpers.rb
@@ -1,11 +1,13 @@
 include Nanoc3::Helpers::Breadcrumbs
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Rendering
 
 def create_tag_pages
   tag_set(items).each do |tag|
     item = Nanoc3::Item.new(
-      "= render('_tag_page', :tag => '#{tag}')",           # use locals to pass data
+      #"= render('_tag_page', :tag => '#{tag}')",           # use locals to pass data
+      render('_tag_page', :tag => tag),           # use locals to pass data
       { :title => "Category: #{tag}", :is_hidden => true}, # do not include in sitemap.xml
       "/rants/tags/#{tag}/",                               # identifier
       :binary => false
@@ -25,4 +27,16 @@ def tag_set(items=nil)
   tags.to_a
 end
 
+def items_for_tag(tag)
+  tagitems = Array.new
+  items.each do |i|
+    next if i[:tags].nil?
+    i[:tags].each do |t| 
+      tagitems << t
+#      if tag == t 
+#        tagitems << i 
+#      end 
+    end
+  end
+end
 
