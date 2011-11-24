763007cb48c0c362280b710ad719fae875628de3
NightSpawN <nsn@nightspawn.com>
2011-11-25 00:58:40 +0100

    more experiments

--- nightspawn.com/static/js/script.js
+++ nightspawn.com/static/js/script.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
   // fancybox
   $("a.gallerylink").fancybox();
 
-
+  // render any picasa streams
+  $(".picasaStream").picasaStream();
 });
 
 
@@ -48,4 +49,30 @@ function showCurrentNavGray() {
 
 function hideCurrentNavGray() {
   $("#nav_" + currentNav + "-gray").hide();
+}
+
+$.fn.picasaStream = function() {
+  this.append("stream! ");
+  var albumID = this.attr("data-albumid");
+  // https://github.com/alanhamlett/jQuery-Picasa-Gallery
+  $.getJSON("https://picasaweb.google.com/data/feed/api/user/nightspawn.com/albumid/" + albumID + "?kind=photo&access=public&alt=json&thumbsize=220c
+    function(data){
+      for (var i = 0; i < data.feed.entry.length; i++) {
+        var pic = data.feed.entry[i];
+
+        var img = $("<img/>");
+        img.attr("class", "picasaimg");
+        img.attr("alt", pic.summary.$t);
+        img.attr("src", pic.media$group.media$thumbnail[0].url);
+
+        var a = $("<a/>");
+        a.attr("href", pic.media$group.media$content[0].url);
+        a.attr("class", "gallerylink");
+        a.attr("rel", "picasa_" + albumID);
+        a.attr("alt", pic.summary.$t);
+        a.append(img);
+        a.appendTo(this);
+      }
+    });
+
 }
